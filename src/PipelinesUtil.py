6f3d66e3a34ff65f7e8b73e94c38f48134e2899c
Dchouras <dchouras@hal.biosci.arizona.edu>
2013-09-04 00:26:48 -0700

    added delete block for utility script

--- src/PipelinesUtil.py
+++ src/PipelinesUtil.py
@@ -8,7 +8,14 @@ def main():
 
 	parser.add_argument("-r", "--runTag", help="Runtag for the pipeline instance")
 
+	parser.add_argument("flag", help="Runtag for the pipeline instance")
+
+	#parser.add_argument("delete", help="Runtag for the pipeline instance")
+
 	args= parser.parse_args()
+
+	if args.runTag == None:
+		raise argparse.ArgumentTypeError("runTag is required to query status or delete jobs of any pipeline.")
 
 	api = FoundationApi.FoundationApi()
 	# Put in your iPlant userid and password
@@ -20,7 +27,7 @@ def main():
 
 	job_status = ''
 
-	if args.runTag != None :
+	if args.flag == 'status' :
         	# returns status of all the jobs for particular run tag
 		job_status = api.past_jobs()
 
@@ -28,6 +35,16 @@ def main():
 			if result['name'] == args.runTag:
 				#print str(result['id'])	
 				print('Job ID: ' + str(result['id']) + '  status = ' + result['status'])
+
+
+	elif args.flag == 'delete' :
+		# returns status of all the jobs for particular run tag
+                job_status = api.past_jobs()
+
+                for result in job_status['result']:
+                        if result['name'] == args.runTag:
+                                api.kill_job(str(result['id']))
+	
 
 if __name__ == "__main__":
     main()
