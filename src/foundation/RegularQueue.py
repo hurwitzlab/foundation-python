02c2a4da5dbe84612f7efa33e29dff511edc48d0
Darren Boss <darren.boss@gmail.com>
2013-12-14 15:58:23 -0500

    Another attempt at fixing

--- src/foundation/RegularQueue.py
+++ src/foundation/RegularQueue.py
@@ -97,11 +97,11 @@ class RegularQueue():
             try:
                 d1 = self.fapi.authexpires
                 d2 = datetime.now()
-                if (d1 - d2) < timedelta(hours=2):
-                    print 'Attempting to super authenticate'
-                    self.fapi.super_authenticate('imicrobe')
-                    if self.verbose:
-                        print 'Authentication token renewed'
+                #if (d1 - d2) < timedelta(hours=2):
+                print 'Attempting to super authenticate'
+                self.fapi.super_authenticate('imicrobe')
+                if self.verbose:
+                    print 'Authentication token renewed'
                 print 'get_monitor_job'
                 job = self.get_monitor_job()
                 print 'update_status()'
